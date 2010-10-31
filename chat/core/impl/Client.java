c3725e7413421c45e1508b38385fcad83f5416c1
raphitheman@gmail.com <raphitheman@gmail.com@62f41db2-1e06-9d71-8ce2-9a41029f5cd1>
2010-10-31 15:22:00 +0000

    Port sicherheit...blaaa

--- chat/core/impl/Client.java
+++ chat/core/impl/Client.java
@@ -20,11 +20,10 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Arrays;
 import java.util.Vector;
-
+import java.util.Date; 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.PropertyConfigurator;
-
 import core.ChatEventListener;
 import chatsession.ChatClientService;
 import chatsession.ChatClientServiceFactory;
@@ -32,9 +31,6 @@ import chatsession.ex.ChatServiceException;
 import chatsession.impl.ChatClientServiceFactoryImpl;
 import chatsession.impl.ChatClientServiceImpl;
 import chatsession.pdu.ChatAction;
-
-import java.util.Date; 
-import java.text.*; 
 
 public class Client implements ChatEventListener {
 	
@@ -50,14 +46,9 @@ public class Client implements ChatEventListener {
 	private static Log log = LogFactory.getLog(Client.class);
 	private ServerCommunicator server;
 	
-	//Erzeugen des Loginframes. Festlegen der Elemente die beim Login benötigt werden, sowie Größe,
-	//Farbe usw. Desweiteren wird hier die Funktionalitäten des Loginframes festgelegt.
-    @SuppressWarnings("Textfeld diable")
-    //TODO: Größe + Farbschema ändern + (Für IP evtl dropdown mit Favoriten) + (Evtl Registrierung am Server mit
-    //Password generierung) -- Status: vorrübergehend Fertig ! 
+	
+	//Erzeugen des Loginframes.
 	private void showLoginFrame() {
-    	
-    	//Erzeugen des Loginframes (Layout,Größe,Form)
         loginFrame = new Frame();
         loginFrame.setLayout(new GridLayout(4, 2, 0, 20));    // 4 rows, 2 columns, gaps
         loginFrame.setResizable(false);
@@ -65,14 +56,12 @@ public class Client implements ChatEventListener {
         loginFrame.setBackground(Color.BLACK);
         loginFrame.setForeground(Color.WHITE);
         
-        //Hinzufügen vom Textfeld mit dem Label "Name"
         loginFrame.add(new Label("Name:"));
         nameField = new TextField(30);
         loginFrame.add(nameField);
         nameField.setForeground(Color.BLACK);
 	    enter(nameField, true);
         
-	    //Hinzufügen vom Textfeld mit dem Label "IP"
         loginFrame.add(new Label("IP:"));
         ipField = new TextField(30);
 		ipField.setText("127.0.0.1");
@@ -80,7 +69,6 @@ public class Client implements ChatEventListener {
         ipField.setForeground(Color.BLACK);
 	    enter(ipField, true);
         
-	    //Hinzufügen vom Textfeld mit dem Label "Port"
         loginFrame.add(new Label("Port:"));
         portField = new TextField(30);
 		portField.setText("50000");
@@ -88,7 +76,6 @@ public class Client implements ChatEventListener {
         portField.setForeground(Color.BLACK);
 	    enter(portField, true);
 	    
-	    //Erzeugen vom "Einloggen" button ( inkl. Mouselistener )
 		Button loginButton = new Button("logMEin");
 		loginButton.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent me) {
@@ -96,31 +83,27 @@ public class Client implements ChatEventListener {
 			}
 		});
 		
-		//Hinzufügen vom "Login Button" 
         loginFrame.add(loginButton);
         loginButton.setForeground(Color.BLACK);
 	    
-        //Funktion macht es möglich das Fenster über X zu schließen
         loginFrame.addWindowListener(new WindowAdapter(){
 	        public void windowClosing(WindowEvent we){
 	          System.exit(0);
 	        }
 	      });
         
-        //Hinzufügen des Errorfeldes 
         errorField = new TextField(50);
+        errorField.enable(false);
         loginFrame.add(errorField);
         errorField.setBackground(Color.PINK);
         errorField.setForeground(Color.BLACK);
-        errorField.enable(false);
 
-        //Sichtbarkeit des ganzen Frames sowie Aussehen feslegen
         loginFrame.pack();
         loginFrame.setVisible(true);
     }
     
-    //Status: Vorrübergehend Fertig !
-    //TODO: andere Schriftarten wählen + URL´s schön einbinden + smiley Button
+
+	//Erzeugen des Chatframes
 	private void showChatFrame() {
         Panel usersPanel = new Panel();
         Color(usersPanel);
@@ -140,7 +123,6 @@ public class Client implements ChatEventListener {
         chatArea.setForeground(Color.WHITE);
         chatArea.setEditable(false);
         chatPanel.add(chatArea, BorderLayout.SOUTH);
-        
         
         Panel ownPanel = new Panel();
         Color(ownPanel);
@@ -177,17 +159,20 @@ public class Client implements ChatEventListener {
         chatFrame.setVisible(true);
     }
 	
-//eigene Hilfsmethode Uhrzeit (TODO FÜR SERVERCOMMUNICATOR + GETTER)
-//	public static String time(){
-//	DateFormat shortTime = DateFormat.getTimeInstance(DateFormat.SHORT); 
-//	return shortTime.format(new Date()).toString(); 
-//	}
+	
+	//eigene Hilfsmethode Uhrzeit (FÜR SERVERCOMMUNICATOR + GETTER)
+/*	public static String time(){
+	DateFormat shortTime = DateFormat.getTimeInstance(DateFormat.SHORT); 
+	return shortTime.format(new Date()).toString(); 
+	}*/
+
 	
 	//eigene Hilfsmethode Farbe
 	private void Color(Panel ty) {
         ty.setBackground(Color.BLACK);
         ty.setForeground(Color.WHITE);
 	}
+	
 	
 	//eigene Hilfsmethode Enter-Taste
     private void enter(TextField field, final boolean login) {
@@ -208,43 +193,41 @@ public class Client implements ChatEventListener {
         });
 	}
     
+    
     //eigene Hilfsmethode zum Check ob Buchstaben oder Zahlen in IP/Port - nicht perfekt
-    public boolean containsOnlyNumbers(String str, String kind) {
+    public boolean ipCheck(String str) {
         
         if (str == null || str.length() == 0)
             return false;
         
         for (int i = 0; i < str.length(); i++) {
-
-        	if(kind == "ip") {
+        	
         		if (!Character.isDigit(str.charAt(i)) && str.charAt(i) != '.') 
         			return false;
         	}
-        	else {
-        		if (!Character.isDigit(str.charAt(i))) 
-        			return false;
-        	}
-        }
         
         return true;
     }
 
-    @Override // Status : grundsätzlich Fertig !
+
+    //Nachricht und Name werden empfangen und zusammen mit der Serverzeit im Chat eingetragen
     public void onMessage(String username, String message) {
     	if(message.contains(";)")) { //TODO: Erster Test: Smileys ersetzen und Schimpfwortfilter
-    		message.replaceAll(";)", ";-)");
+    		message.replaceAll(";)", ";-)");  //Funktionsweise bei Laufzeit testen und ggf. über ENUM-Klassen steuern
     	}
         chatArea.append("(" + server.getTime() + ")" + " " + username + ": " + message + "\n");
         chatFrame.setVisible(true);
     }
-    
-    @Override //TODO: Status : ?
+
+
+    //Userliste wird an fillUserList weitergegeben
     public void onUserListUpdate(String[] userList) {
         fillUserList(new Vector<String>(Arrays.asList(userList)));
         chatFrame.setVisible(true);
     }
+
     
-    //Status: vorerst Fertig !
+    //aktualisiert die Userliste
     private void fillUserList(Vector<String> names) {
         userList.removeAll();
         for (int i = 0; i < names.size(); i++)
@@ -253,61 +236,64 @@ public class Client implements ChatEventListener {
     }   
     
     
-    @Override //Status: vorerst Fertig!
+    //Wenn der Name schon vorhanden ist -> Errormeldung im Chat
     public void onAction(int actionId, String reserved) {
         if (actionId == ChatAction.CHATACTION_USERNAME_SCHON_VERGEBEN) {
             chatArea.append("Der Username ist schon vergeben. Bitte loggen Sie sich aus und starten Sie die Anwendung neu!");
         }
     }
 
-    // Status: voerst Fertig !
+
+    //Ctor, Logdatei einstellen + Loginframe anzeigen
     public Client() {
         PropertyConfigurator.configureAndWatch("log4j.properties", 60 * 1000);
         showLoginFrame();
 		log.debug("<< Chat gestartet >>");
     }
 
-    //MAIN ( Status: voerst Fertig ! )
+    //MAIN
     public static void main(String[] args) throws AWTException {
         new Client();
     }
 
-    //TODO: neue Login-Methode (vorher LoginListener) ( Status: überarbeiten - Fehlerhandling bei IP und beim Port )
+    //Login-Methode (vorher LoginListener) ( TODO: überarbeiten - Fehlerhandling bei IP und beim Port )
     public void logMEin() {
             boolean ok = true;
         	String error = "";
+        	userName = nameField.getText();
+            userIP = ipField.getText();
+            userPort = portField.getText();
+        	int port = Integer.parseInt(userPort); //String -> int
         	
         	if (nameField.getText().isEmpty()) {
             error += " Name ";
             ok = false;
             }
 
-            if (ipField.getText().isEmpty() | !containsOnlyNumbers(ipField.getText(),"ip")) {
+            if (userIP.isEmpty() | !ipCheck(userIP)) {
             	error += "IP ";
             	ok = false;
-            }
+	            }
 
-            if (portField.getText().isEmpty() | !containsOnlyNumbers(portField.getText(),"port")) { //absichern dass es nur zahlen sind usw...
+            if (userPort.isEmpty() || userPort.length() <= 5 || port >= 00000 || port <= 65535) {
             	error += "Port ";
             	ok = false;
             }
-
             if (ok) {
-        	userName = nameField.getText();
-            userIP = ipField.getText();
-            userPort = portField.getText();
-            int port = Integer.parseInt(userPort); //String -> int
+
+//          int port = Integer.parseInt(userPort); //String -> int
 
             loginFrame.dispose();
             showChatFrame();
-            login(userName, userIP, port);
+            login(userName,userIP,port);
             }
             else {
             	errorField.setText("Bitte Eingaben kontrollieren:" + error);
             }
         }
     
-    //Methode Login und Logout zuständig  Status: Fertig
+    
+    //Methode Login und Logout zuständig
     class ChatListener extends MouseAdapter {
         public void mouseClicked(MouseEvent e) {
             if (e.getSource() == submitButton) {
@@ -318,10 +304,10 @@ public class Client implements ChatEventListener {
                 System.exit(0);
             }
         }
-        
     }
     
-    //TODO: Status: zu überarbeiten !!
+    
+    //TODO: überarbeiten !! + Port evtl. dynamisch gestalten?
 	public void login(String name, String ip, int port) {
 
 		ChatClientServiceFactory chatclientservicefactory = new ChatClientServiceFactoryImpl();
@@ -329,12 +315,11 @@ public class Client implements ChatEventListener {
 		
 		try {
 			chatclientservicefactory.register(port); //Registriert einen ChatClientService an einem lokalen port und gibt das ChatClientService-Objekt zur+ck
-			chatClientService.create(ip, port, name); //TODO: dynamisch. //Baut eine Session mit dem angegeben Partner auf
+			chatClientService.create(ip, port, name); //Baut eine Session mit dem angegeben Partner auf
 			communicator = new ClientCommunicator(this, chatClientService, name);
 			} catch (ChatServiceException e) {
 				new ChatServiceException("Verbindungsfehler.");
 			}
-
 	}
 
 } // Client
